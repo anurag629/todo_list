38250de5b4fb28abbce14d62067225c2e3b8af00
anurag629 <akvermaav629@gmail.com>
2022-01-06 08:07:13 +0530

    create method to add new task

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskDetail, TaskList
+from .views import TaskDetail, TaskList, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create', TaskCreate.as_view(), name='task-create'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.db.models import fields
 from django.db.models.base import Model
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from .models import Tasks
 
 
@@ -15,3 +19,9 @@ class TaskDetail(DetailView):
     model = Tasks
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Tasks
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
