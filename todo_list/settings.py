045fee538fc36b6cb3caab2c148140643377f510
Your Name <you@example.com>
2022-01-09 20:02:50 +0530

    all done

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -126,11 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'todo_list/static')
-                    ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 LOGIN_URL = 'login'
 
